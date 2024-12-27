00715bb5a862fecb9d63623869b9583b9d369004
Anatoliy <anatolvereschaka@ukr.net>
2024-12-27 18:30:18 +0200

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { lazy, Suspense } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import FireworksComponent from "./components/Fireworks/Fireworks";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
@@ -18,39 +17,12 @@ const MovieReviews = lazy(() =>
 );
 
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false); // Состояние для модального окна
-  const [isConfettiVisible, setIsConfettiVisible] = useState(false); // Состояние для салюта
-
-  useEffect(() => {
-    // Проверка, показывалось ли уже модальное окно
-    const isModalShown = localStorage.getItem("modalShown");
-
-    if (!isModalShown) {
-      // Если модальное окно не показывалось ранее, показываем его через 3 секунды
-      const timer = setTimeout(() => {
-        setIsModalVisible(true);
-        setIsConfettiVisible(true); // Показываем салют, когда открывается модальное окно
-        localStorage.setItem("modalShown", "true"); // Сохраняем в localStorage, что окно показано
-      }, 3000);
-
-      return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
-    }
-  }, []);
-
-  const closeModal = () => {
-    setIsModalVisible(false); // Закрытие модального окна
-    setIsConfettiVisible(false); // Прекращение отображения салюта
-  };
-
   return (
     <ErrorBoundary>
       <Navigation />
       <Suspense fallback={<div>Loading...</div>}>
-        {/* Показываем компонент с салютом только если isConfettiVisible true */}
-        {isConfettiVisible && <FireworksComponent />}
-
         <Routes>
-          <Route path="/" element={<HomePage isModalVisible={isModalVisible} closeModal={closeModal} />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
             <Route path="cast" element={<MovieCast />} />
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,14 +4,11 @@ import MovieList from "../../components/MovieList/MovieList";
 import Loader from "../../components/Loader/Loader";
 import Error from "../../components/Error/Error";
 import css from "./HomePage.module.css";
-import FireworksComponent from "../../components/Fireworks/Fireworks";
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false); // Состояние для модального окна
-  const [isConfettiVisible, setIsConfettiVisible] = useState(false); // Состояние для салюта
 
   useEffect(() => {
     async function fetchData() {
@@ -31,67 +28,14 @@ const HomePage = () => {
     }
 
     fetchData();
-
-    // Показываем модальное окно через 3 секунды
-    const timer = setTimeout(() => {
-      setIsModalVisible(true);
-      setIsConfettiVisible(true); // Показываем салют, когда открывается модальное окно
-    }, 3000);
-
-    // Очистка таймера при размонтировании компонента
-    return () => clearTimeout(timer);
   }, []);
-
-  const closeModal = () => {
-    setIsModalVisible(false); // Закрытие модального окна
-    setIsConfettiVisible(false); // Прекращение отображения салюта
-  };
 
   return (
     <div>
-      {/* Показываем компонент с салютом только если isConfettiVisible true */}
-      {isConfettiVisible && <FireworksComponent />}
-
-      <h2 className={css.title}>Популярные фильмы сегодня</h2>
+      <h2 className={css.title}>Trending today</h2>
       {loading && <Loader />}
       {error && <Error />}
       {movies.length > 0 ? <MovieList movies={movies} /> : <p>No movies</p>}
-
-      {/* Модальное окно */}
-      {isModalVisible && (
-        <div className={css.modal}>
-          <div className={css.modalContent}>
-            <h3>
-              Оксана, Нина, Оля!!!!
-              <br />
-              <br />
-              Happy New Year!!!!
-              <br />
-              <br />
-              В Новий рік бажаю щастя,
-              <br />
-              Щоб усе гаразд було.
-              <br />
-              Гарним буде хай ваш настрій,
-              <br />
-              А в душі — завжди тепло!
-              <br />
-              <br />
-              Щоб складалися всі плани,
-              <br />
-              Щоб везіння вас любило,
-              <br />
-              Нехай рік щасливим стане —
-              <br />
-              Щастя, мудрості і сили!
-              <br />
-              <br />
-              Толик, Юля, Олеся
-            </h3>
-            <button onClick={closeModal}>Close</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
